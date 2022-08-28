a3bfd4806f7977fc6e67922398f6ac935b3e034c
Ted Skolnick <ted.skolnick@gmail.com>
2022-08-28 23:07:56 +0000

    got it working

--- public/content/home.js
+++ public/content/home.js
@@ -1,17 +1,16 @@
-$( function (){
-  
+function load_content( template_path, data_path ){
  let tmpl = '' // Main template HTML
  let tdata = {}; // JSON data object that feeds the template
 
 
  // Load the HTML template
- $.get("/templates/director_list.html", function(d){ // 1
+ $.get(`templates/${template_path}`, function(d){ 
  tmpl = d
  });
 
   
  // Retrieve the server data and then initialize the page
- $.getJSON("/directors", function (d) { 
+ $.getJSON(data_path, function (d) { 
  tdata = d 
  });
 
@@ -21,7 +20,22 @@ $( function (){
  $(document).ajaxStop(function () {
    console.log( tdata  )
  var renderedPage = Mustache.to_html(tmpl, tdata); // 3
- $("body").html(renderedPage);
+ $("#content").html(renderedPage);
  })
 
+  
+}
+
+
+$( function (){
+  $("a").click(function(){
+    alert(0)
+    let director = $(this).attr('director');
+     console.log( 'director click ' + director )
+  });
+  
+  load_content(
+    'director_list.html', 
+    "/directors"
+    )
 }); 
